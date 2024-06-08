c99c7ec63467aa4079346fa7d74ee925681db410
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-06-08 12:53:09 +0300

    [523] [Time Beats: 5.70%] [Memory Beats: 24.60%] - LeetPush

---
+++ 523-Continuous-Subarray-Sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkSubarraySum(vector<int>& nums, int k) {
+    int n = nums.size();
+    map<int, int>mp;
+    int sum = 0;
+    bool found = false;
+    int last = 0;
+    for(int i = 0 ; i < n ; i++){
+        sum += nums[i];
+        int mod = sum % k ;
+        if(mp.count(mod)){
+            if(i - mp[mod] >= 1){
+                found = true;
+                break;
+            }
+        }
+        if(mod == 0){
+            if(i >= 1)
+            {
+                found = true;
+                break;
+            }
+        }
+        if(!mp[(nums[i] + last) % k])
+            mp[(nums[i] + last) % k] = i + 1;
+        else
+            mp[(nums[i] + last) % k] = min(mp[(nums[i] + last) % k], i + 1);
+        last += nums[i];
+    }
+    return found;
+}
+};
