7b52e62919eb18f1d9c8c1a1ea75a60456f6d51d
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-06-18 13:29:03 +0300

    [826] [Time Beats: undefined] [Memory Beats: undefined] - LeetPush

---
+++ 826-Most-Profit-Assigning-Work.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
+        int n = profit.size();
+        vector<pair<int, int>>v(n);
+        for(int i = 0 ; i < n ; i++){
+            v[i] = {difficulty[i], profit[i]};
+        }
+        sort(v.begin(), v.end());
+        for(int i = 1 ; i < n ; i++){
+            v[i].second = max(v[i].second, v[i - 1].second);
+        }
+        int m = worker.size(); 
+        int total = 0;
+        for(int i = 0 ; i < m ; i++){
+            int cur = worker[i];
+            int l = 0 , r = n - 1 ;
+            int ans = -1;
+            while(l <= r){
+                int mid = l + (r - l)/2;
+                int diff = v[mid].first;
+                int val = v[mid].second;
+                if(cur >= diff){
+                    l = mid + 1;
+                    ans = val;
+                }else{
+                    r = mid - 1;
+                }
+            }
+            if(ans != -1)
+                total += ans;
+        }
+        return total;
+    }
+};
