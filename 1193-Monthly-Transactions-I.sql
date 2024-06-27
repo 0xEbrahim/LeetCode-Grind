adfa248125e515b6fbeaa36c551b4050aa576d47
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-06-27 20:27:10 +0300

    [1193] [Time Beats: undefined] - LeetPush

--- 1193-Monthly-Transactions-I.sql
+++ 1193-Monthly-Transactions-I.sql
@@ -1,11 +1,8 @@
-SELECT month,
+SELECT DATE_FORMAT(CAST(trans_date as DATE), "%Y-%m") as month,
  country,
   COUNT(*) AS trans_count,
    SUM(IF(state="approved",1,0)) AS approved_count ,
     SUM(amount) AS trans_total_amount ,
      SUM(IF(state="approved", amount, 0)) AS approved_total_amount 
-FROM (
-    SELECT country, state, amount, DATE_FORMAT(CAST(trans_date as DATE), "%Y-%m") as month
-    FROM Transactions 
-)T1
+ FROM Transactions 
 GROUP BY month, country
