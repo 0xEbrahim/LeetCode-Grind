15e3b6e60814fc8929301126636d473d4619d8d6
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-06-28 12:05:57 +0300

    Update 1559-Detect-Cycles-in-2D-Grid.cpp

--- 1559-Detect-Cycles-in-2D-Grid.cpp
+++ 1559-Detect-Cycles-in-2D-Grid.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
-int n , m ;
-vector<vector<int>>v;
-int dp[71][71][71];
+    int n , m ;
+char cur;
+int dx[4] = {1, -1, 0, 0},
+        dy[4] = {0, 0, 1, -1};
+vector<vector<char>>v;
+bool vis[512][512] = {};
 bool valid(int i , int j){
-    return i >= 0 && i < n && j >= 0 && j < m ;
+    return i < n && i >= 0 && j < m && j >= 0 && v[i][j] == cur ;
 }
-int solve(int i , int j , int r){
-    if(!valid(i, j) || !valid(i , r))
-        return 0;
-    int &ret = dp[i][j][r];
-    if(~ret)
-        return ret;
-    int op1 = 0 , op2 = 0 , op3 = 0 , op4 = 0 , op5 = 0 , op6 = 0, op7 = 0 , op8 = 0, op9 = 0;
-    if(j == r){
-    op1 = max(solve(i + 1,  j - 1,  r - 1) +  v[i][j] ,solve(i + 1,  j - 1,  r - 1) + v[i][r]);
-    op2 = max(solve(i + 1,  j - 1,   r)     +       v[i][j] ,solve(i + 1,  j - 1,   r) +  v[i][r]);
-    op3 = max(solve(i + 1,  j - 1,   r + 1) +     v[i][j] , solve(i + 1,  j - 1,   r + 1) + v[i][r]);
-    op4 = max(solve(i + 1,  j,        r - 1) +    v[i][j] , solve(i + 1,  j,       r - 1) + v[i][r]);
-    op5 = max(solve(i + 1,  j,       r)     +         v[i][j] , solve(i + 1,  j,       r) + v[i][r]);
-    op6 = max(solve(i + 1,  j,        r + 1) +    v[i][j] ,solve(i + 1,  j,        r + 1) + v[i][r]);
-    op7 = max(solve(i + 1,  j + 1,   r - 1) +    v[i][j] , solve(i + 1,  j + 1,    r - 1) +  v[i][r]);
-    op8 = max(solve(i + 1,  j + 1,    r)     +         v[i][j] ,solve(i + 1,  j + 1,   r)     +  v[i][r]);
-    op9 = max(solve(i + 1,  j + 1,   r + 1) +    v[i][j] ,solve(i + 1,  j + 1,  r + 1) + v[i][r]);
-    }else{
-    op1 = solve(i + 1,  j - 1,   r - 1) +    v[i][j] + v[i][r];
-    op2 = solve(i + 1,  j - 1,   r)     +       v[i][j] + v[i][r];
-    op3 = solve(i + 1,  j - 1,  r + 1) +     v[i][j] + v[i][r];
-    op4 = solve(i + 1,  j,        r - 1) +    v[i][j] + v[i][r];
-    op5 = solve(i + 1,  j,        r)     +         v[i][j] + v[i][r];
-    op6 = solve(i + 1,  j,       r + 1) +    v[i][j] + v[i][r];
-    op7 = solve(i + 1,  j + 1,    r - 1) +    v[i][j] + v[i][r];
-    op8 = solve(i + 1,  j + 1,   r)     +         v[i][j] + v[i][r];
-    op9 = solve(i + 1,  j + 1,    r + 1) +    v[i][j] + v[i][r];
+bool detect(int i_nd, int j_nd, int i_par, int j_par){
+    vis[i_nd][j_nd] = true;
+    for(int i = 0 ; i < 4 ; i++){
+        int i_child = i_nd + dx[i];
+        int j_child = j_nd + dy[i];
+        if(valid(i_child, j_child) && !vis[i_child][j_child]){
+            if(detect(i_child, j_child, i_nd, j_nd))
+                return true;
+        }else if(valid(i_child, j_child) && i_child != i_par && j_child != j_par)
+            return true;
     }
-    return ret = max({op1, op2 , op3 , op4 , op5 , op6 , op7, op8, op9});
+    return false;
 }
-    int cherryPickup(vector<vector<int>>& grid) {
-        n = grid.size();
-        m = grid[0].size();
-        v = grid;
-        memset(dp, -1, sizeof dp);
-        return solve(0,0,m-1);
+bool containsCycle(vector<vector<char>>& grid) {
+    n = grid.size();
+    m = grid[0].size();
+    v = grid;
+    bool cycle = false;
+    for(int i = 0 ; i < n ; i++){
+        for(int j = 0 ; j < m ; j++){
+            if(!vis[i][j]){
+                cur = v[i][j];
+                cycle |= detect(i,j,-1,-1);
+            }
+        }
     }
+    return cycle;
+}
 };
