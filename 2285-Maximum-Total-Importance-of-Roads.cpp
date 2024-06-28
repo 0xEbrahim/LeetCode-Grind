cf974dfa0490c25ae2f7292b55662056ce660e7b
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-06-28 12:18:28 +0300

    [2285] [Time Beats: 56.55%] [Memory Beats: 46.21%] - LeetPush

---
+++ 2285-Maximum-Total-Importance-of-Roads.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        vector<long long>deg(n + 5);
+        for(int i = 0 ; i < roads.size() ; i++){
+            deg[roads[i][0]]++;
+            deg[roads[i][1]]++;
+        }
+        vector<pair<long long, long long>>values;
+        for(int i = 0 ; i < n ; i++){
+            values.push_back({deg[i], i});
+        }
+        sort(values.rbegin(), values.rend());
+        vector<long long>importance(n + 2);
+        int val = n;
+        for(int i = 0 ; i < n ; i++)
+            importance[values[i].second] = val--;
+        long long ans = 0;
+        for(int i = 0 ; i < roads.size() ; i++){
+            ans += importance[roads[i][0]];
+            ans += importance[roads[i][1]];
+        }
+        return ans;
+    }
+};
