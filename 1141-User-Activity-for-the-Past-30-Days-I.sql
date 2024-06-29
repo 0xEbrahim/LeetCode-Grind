5d817ea307f01961ddff99c054979bfbaa398b39
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-06-29 20:21:23 +0300

    [1141] [Time Beats: undefined] - LeetPush

--- 1141-User-Activity-for-the-Past-30-Days-I.sql
+++ 1141-User-Activity-for-the-Past-30-Days-I.sql
@@ -1,5 +1,4 @@
 SELECT activity_date as day, COUNT(DISTINCT user_id) as active_users 
 FROM Activity 
-WHERE DATEDIFF("2019-07-27",activity_date) < 30 AND activity_date<="2019-07-27"
+WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
 GROUP BY activity_date 
-HAVING COUNT(DISTINCT user_id) > 0
