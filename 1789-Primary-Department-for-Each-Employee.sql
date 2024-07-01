888bc99a8e37cf411664c8f2955a3488541f643a
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-07-01 13:36:06 +0300

    [1789] [Time Beats: undefined] - LeetPush

--- 1789-Primary-Department-for-Each-Employee.sql
+++ 1789-Primary-Department-for-Each-Employee.sql
@@ -1,8 +1,8 @@
 SELECT e.employee_id , e.department_id 
 FROM Employee e
-WHERE e.primary_flag='Y' OR (
-    (
-    SELECT SUM(IF(e.employee_id=employee_id,1,0))
-    FROM Employee
-    ) = 1 AND e.primary_flag='N'
-)
+WHERE e.primary_flag='Y' 
+UNION
+SELECT e.employee_id , e.department_id 
+FROM Employee e
+GROUP BY employee_id
+HAVING COUNT(employee_id)=1
