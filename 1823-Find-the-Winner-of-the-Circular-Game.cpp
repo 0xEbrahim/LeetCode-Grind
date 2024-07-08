b0485aa7064eb0dd54cada40e32e92a020461d56
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-07-08 13:33:37 +0300

    [1823] [Time Beats: 75.45%] [Memory Beats: 92.77%] - LeetPush

--- 1823-Find-the-Winner-of-the-Circular-Game.cpp
+++ 1823-Find-the-Winner-of-the-Circular-Game.cpp
@@ -1,40 +1,13 @@
 class Solution {
 public:
+    int helper(int n,int k){
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            ans = (ans + k) % i;
+        }
+        return ans;
+    }
     int findTheWinner(int n, int k) {
-    vector<bool>vis(n + 1, false);
-    int cnt = 0, count = 0;
-    for(int i = 1 ; i <= n ; i++){
-        if(vis[i]){
-            if(i == n){
-            i = 0;
-        }
-            continue;
-            }
-        cnt++;
-         if(count == n - 1)
-            break;
-        if(cnt == k){
-            vis[i] = true;
-            cnt = 0;
-            count++;
-        }
-
-        if(i == n){
-            i = 0;
-        }
+        return helper(n,k)+1;  
     }
-    int ret = 0;
-    for(int i = 1 ; i <= n ; i++){
-        if(!vis[i]){
-            ret = i;
-            break;
-        }
-    }
-    return ret;
-}
-// 1 2 3 4 5
-// 1 2 4 5
-// 2 4 5
-// 2 4
-// 4
 };
