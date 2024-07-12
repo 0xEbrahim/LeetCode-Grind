1418c2ce849869952b9721db0467d9cb65ae313d
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-07-12 17:53:20 +0300

    [938] [Time Beats: 42.76%] [Memory Beats: 9.77%] - LeetPush

---
+++ 938-Range-Sum-of-BST.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool findValue(int val,TreeNode*root){
+        if(root == NULL)
+            return false;
+        if(root -> val == val)
+            return true;
+        bool right = false, left = false;
+        if(val > root -> val)
+            right = findValue(val, root -> right);
+        else
+            left = findValue(val, root -> left);
+        return right || left ;
+    }
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        int ret = 0;
+        for(int i = low ; i <= high ; i++){
+            TreeNode*tmp = root;
+            if(findValue(i, root)){
+                root = tmp;
+                ret += i;
+            }
+        }
+        return ret;
+    }
+};
