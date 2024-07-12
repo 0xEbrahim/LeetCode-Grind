0bd8e3b80e07bb414a32a1b77b429c780cef5080
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-07-12 17:45:03 +0300

    [2433] [Time Beats: 51.53%] [Memory Beats: 35.58%] - LeetPush

--- 2433-Find-The-Original-Array-of-Prefix-Xor.cpp
+++ 2433-Find-The-Original-Array-of-Prefix-Xor.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
-        vector<int>ret;
         int n = pref.size();
-        ret.push_back(pref[0]);
-        for(int i = 1 ; i < n ; i++)
-            ret.push_back(pref[i] ^ pref[i - 1]);
-        return ret;
+        int prev = pref[0];
+        for(int i = 1 ; i < n ; i++){
+            int tmp = pref[i];
+            pref[i] = (pref[i] ^ prev);
+            prev = tmp;
+            }
+        return pref;
     }
 };
