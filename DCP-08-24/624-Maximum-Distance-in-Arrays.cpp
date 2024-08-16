9dadb7d58682b99b2d87ff13edd351887f87981e
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-08-16 21:32:36 +0300

    [624] [Time Beats: 99.53%] [Memory Beats: 25.00%] - LeetPush

--- DCP-08-24/624-Maximum-Distance-in-Arrays.cpp
+++ DCP-08-24/624-Maximum-Distance-in-Arrays.cpp
@@ -5,24 +5,29 @@ public:
         int n = v.size();
         int m = v[0].size();
         vector<pair<int, int>>mins(n), maxs(n);
+        pair<int, int> mn, mx;
+        mn = {1e5 + 1, 0};
+        mx = {-1e6 + 1, 0};
         for(int i = 0 ; i < n ; i++){
             mins[i] = {1e5, i };
             maxs[i] = {-1e5, i};
             for(int j = 0 ; j < v[i].size() ; j++){
                 mins[i] = { min(mins[i].first, v[i][j]) , i };
                 maxs[i] = { max(maxs[i].first, v[i][j]), i };
+                if(mins[i].first < mn.first)
+                    mn = mins[i];
+                if(maxs[i].first > mx.first)
+                    mx = maxs[i];
             }
         }
     int ret1 = -1e5, ret2 = -1e5;
-    sort(mins.begin(), mins.end());
-    sort(maxs.rbegin(), maxs.rend());
     int l1 = 0, l2 = 0;
     for(int i = 0 ; i < n ; i++){
-        if(mins[0].second != maxs[i].second){
-            ret1 = max(ret1, abs(mins[0].first - maxs[i].first));
+        if(mn.second != maxs[i].second){
+            ret1 = max(ret1, abs(mn.first - maxs[i].first));
         }
-        if(mins[i].second != maxs[0].second){
-            ret2 = max(ret2, abs(mins[i].first - maxs[0].first));
+        if(mins[i].second != mx.second){
+            ret2 = max(ret2, abs(mins[i].first - mx.first));
         }
     }
     return max(ret1, ret2);
