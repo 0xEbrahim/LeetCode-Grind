36f61a401b4c775f9bbb9c6cd730fbf264dc8655
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-08-17 11:06:48 +0300

    [424] [Time Beats: 60.01%] [Memory Beats: 68.08%] - LeetPush

--- 424-Longest-Repeating-Character-Replacement.cpp
+++ 424-Longest-Repeating-Character-Replacement.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int freq[26] = {};
         int n = s.size();
-        int ret = 0;
+        int freq[26] = {};
         int l = 0 , r = 0 , mx_len = 0;
         int mx_freq = 0;
-        for(; r < n ; r++){
+        while(r < n){
             freq[s[r] - 'A']++;
             mx_freq = max(mx_freq, freq[s[r] - 'A']);
             if(r - l + 1 - mx_freq > k){
@@ -15,7 +14,8 @@ public:
                 l++;
                 mx_freq = *max_element(freq, freq + 26);
             }
+            r++;
         }
-    return max(r - l , mx_len);
+        return max(mx_len, r - l);
     }
 };
