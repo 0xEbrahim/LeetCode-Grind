a2a3bc7ac1c1bc2a025b77bac0bc5402c3d141f0
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-08-18 13:50:20 +0300

    [219] [Time Beats: 15.51%] [Memory Beats: 11.67%] - LeetPush

---
+++ 219-Contains-Duplicate-II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& v, int k) {
+        if(k == 0)return false;
+        int l = 0, r = 0;
+        int n = v.size();
+        map<int, int>mp;
+        while(r < n){
+            if(abs(r - l) > k)l++;
+            if(mp.count(v[r]) && mp[v[r]] >= r - k) return true;
+            mp[v[r]] = r;
+            r++;
+        }
+        return false;
+    }
+};
