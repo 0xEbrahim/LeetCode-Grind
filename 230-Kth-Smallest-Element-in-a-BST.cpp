6e8a2beeae5de7e27764957b3de2fe3bec8501c7
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-08-20 19:35:25 +0300

    [230] [Time Beats: 8.99%] [Memory Beats: 74.88%] - LeetPush

--- 230-Kth-Smallest-Element-in-a-BST.cpp
+++ 230-Kth-Smallest-Element-in-a-BST.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-vector<int>v;
-    void inorder(TreeNode* root, int& k) {
-        if (!root) return;
-        inorder(root->left, k);
-       v.push_back(root -> val);
-        inorder(root->right, k);
+    void inorder(TreeNode*root, int &k){
+        if(root == NULL) return;
+        inorder(root -> left, k);
+        if(--k == 0) res = root -> val;
+        inorder(root -> right, k);
     }
-    
     int kthSmallest(TreeNode* root, int k) {
         inorder(root, k);
-        return v[k - 1];
+        return res;
     }
     
 private:
