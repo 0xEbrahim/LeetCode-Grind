28270b69684314fad99b983363248d7d3b55f5d0
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-08-21 19:46:41 +0300

    [5] [Time Beats: 19.27%] [Memory Beats: 34.36%] - LeetPush

---
+++ 5-Longest-Palindromic-Substring.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    std::string longestPalindrome(std::string s) {
+        int n = s.size();
+        vector<vector<bool>>dp(n + 1, vector<bool>(n + 1, 0));
+        for(int i = 0 ; i < n ; i++)
+            dp[i][i] = true;
+        for(int i = n - 1 ; ~i ; i--){
+            for(int j = 0 ; j < n ; j++){
+                if(i >= j){
+                    dp[i][j] = true;
+                    continue;
+                }
+                if(s[i] == s[j]){
+                    dp[i][j] = dp[i + 1][j - 1];
+                }
+            }
+        }
+        int l = 0 , r = 0, mx = 0;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i ; j < n ; j++){
+                if(dp[i][j] == 1){
+                    if(j - i + 1 > mx){
+                        mx = j - i + 1;
+                        l = i;
+                        r = j;
+                    }
+                }
+            }
+        }
+        string ret = "";
+        for(int i = l ; i <= r ; i++) ret += s[i]; 
+        return ret;
+    }
+};
