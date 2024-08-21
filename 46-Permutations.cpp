85e0073c3e6678918480e7d9159be7804a7fb503
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-08-21 15:28:40 +0300

    [46] [Time Beats: 52.59%] [Memory Beats: 22.98%] - LeetPush

---
+++ 46-Permutations.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>>res;
+    void perm(vector<int>&nums, vector<int>&sub, vector<bool>&vis){
+        if(sub.size() == nums.size()) {
+            res.push_back(sub); return;
+        }
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(!vis[i]){
+                sub.push_back(nums[i]);
+            vis[i] = true;
+            perm(nums, sub, vis);
+            vis[i] = false;
+            sub.pop_back();}
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int>g = {};
+        vector<bool> vis(nums.size() + 1, false);
+        perm(nums, g, vis);
+        return res;
+    }
+};
