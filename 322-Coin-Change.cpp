eef781c88fc4a847f88db2a5117f45946c6b1361
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-08-21 20:06:25 +0300

    [322] [Time Beats: 53.63%] [Memory Beats: 58.92%] - LeetPush

--- 322-Coin-Change.cpp
+++ 322-Coin-Change.cpp
@@ -1,33 +1,18 @@
 class Solution {
 public:
-    vector<long long>dp,coin;
-    int n;
-    long long mn;
-    long long go(int have){
-        if(have == 0){
-            return 0;
+    int go(vector<int>&dp, vector<int>&coins, int target){
+        if(target < 0) return 1e6;
+        if(target == 0) return 0;
+        int &ret = dp[target];
+        if(~ret) return ret;
+        ret = INT_MAX;
+        for(int i = 0 ; i < coins.size() ; i++){
+            ret = min(ret, 1 + go(dp, coins, target - coins[i]));
         }
-        if(have < mn)
-            return 1e15;
-        long long &ret = dp[have];
-        if(~ret)
-            return ret;
-        ret = 1e16;
-       for(int i = 0 ; i < n ; i++){
-            if(have >= coin[i]){
-                ret = min(ret, go(have - coin[i]) + 1);
-            }
-       }
         return ret;
     }
     int coinChange(vector<int>& coins, int amount) {
-        n = coins.size();
-        coin.resize(n);
-        mn = LLONG_MAX;
-        for(int i = 0 ; i < n ; i++)
-            coin[i] = coins[i], mn = min(mn , coin[i]);
-        dp.resize(amount + 1 , -1);
-        long long ret = go(amount);
-        return (ret >= 1e15 ? -1 : ret);
+       vector<int>dp(amount + 1, -1);
+       return go(dp, coins, amount) >= 1e6 ? -1 : go(dp, coins, amount);
     }
 };
