242e6bbe5355636d3687ed3e1cbe54709a67ac98
Ebrahim El-Sayed <107937836+0xEbrahim@users.noreply.github.com>
2024-08-21 15:44:03 +0300

    [39] [Time Beats: 100.00%] [Memory Beats: 49.05%] - LeetPush

--- 39-Combination-Sum.cpp
+++ 39-Combination-Sum.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    set<vector<int>>st;
-    void perm(vector<int>&nums, vector<int>&comp, int target){
+    vector<vector<int>>st;
+    void perm(int id, vector<int>&nums, vector<int>&comp, int target){
         if(target < 0) return;
         if(target == 0){
-            vector<int>vv = comp;
-            sort(vv.begin(), vv.end());
-            st.insert(vv);
+            st.push_back(comp);
             return;
         }
-        for(int i = 0 ; i < nums.size() ; i++){
+        for(int i = id ; i < nums.size() ; i++){
             comp.push_back(nums[i]);
-            perm(nums, comp, target - nums[i]);
+            perm(i, nums, comp, target - nums[i]);
             comp.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> g ={};
-        perm(candidates, g, target);
-        vector<vector<int>>v(st.begin(), st.end());
-        return v;
+        sort(candidates.begin(), candidates.end());
+        perm(0, candidates, g, target);
+        return st;
     }   
 };
